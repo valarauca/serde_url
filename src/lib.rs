d2ac70454edff5e4e28cc7cf1ea565c43f921ea4
William Laeder <codylaeder@gmail.com>
2019-02-25 15:45:19 -0800

    will need to touch up the trait, maybe add some data

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::str;
 use std::fmt;
 use std::sync;
 use std::hash;
+use std::path;
 
 extern crate url;
 extern crate serde;
@@ -11,7 +12,84 @@ mod errors;
 pub use self::errors::{UrlFault};
 mod internal;
 use self::internal::PrivateUrl;
-pub use self::internal::{Host,QueryData,QueryValues};
+pub use self::internal::{Origin,Host,QueryData,QueryValues};
+
+/// `UrlOp` allows for exposing all the functionality of
+/// a `Url` type without exposing internal field
+/// semantics. 
+///
+/// This lets types _inheriate_ `Url` functionality
+/// for the fee of just storing a `Url` field, and writing
+/// the `impl AsRef<Url` boilerplate.
+pub trait UrlOp: AsRef<Url> {
+
+    /// `get_string` just returns a string
+    fn get_string<'a>(&'a self) -> &'a str {
+        self.as_ref().data.get_string()
+    }
+
+    /// `get_scheme` returns the URL's scheme
+    fn get_scheme<'a>(&'a self) -> &'a str {
+        self.as_ref().data.get_scheme()
+    } 
+
+    /// `get_username` returns the percentage decoded username
+    /// if one is present.
+    fn get_username<'a>(&'a self) -> Option<&'a str> {
+        self.as_ref().data.get_username()
+    }
+
+    /// `get_password` returns the percentage decoded password
+    /// if one is present.
+    fn get_password<'a>(&'a self) -> Option<&'a str> {
+        self.as_ref().data.get_password()
+    }
+
+    /// `get_host` returns host information. This maybe a domain
+    /// name, or IP address. You are encouraged to inspect the
+    /// value if interested.
+    fn get_host<'a>(&'a self) -> Option<Host<&'a str>> {
+        self.as_ref().data.get_host()
+    }
+
+    /// `get_port` returns host information about the `port`.
+    fn get_port(&self) -> Option<u16> {
+        self.as_ref().data.get_port()
+    }
+
+    /// `get_origin` returns an a _non-opaque_ origin. If one
+    /// is present. This contains the `host` and `port`, as
+    /// well as `scheme` information.
+    fn get_origin<'a>(&'a self) -> Option<Origin<'a>> {
+        self.as_ref().data.get_origin()
+    }
+
+    /// `get_path` returns the `path` component of the URL
+    ///
+    /// # Note
+    ///
+    /// attempts to decode the percentage encoding if any
+    /// is present.
+    fn get_path<'a>(&'a self) -> Option<&'a path::Path> {
+        self.as_ref().data.get_path()
+    }
+
+    /// `get_path_str` returns the `path` component of the URL, as a `str` vs `Path`,
+    /// which maybe preferable in some scenarios.
+    ///
+    /// # Note
+    ///
+    /// attempts to decode the percentage encoding if any
+    /// is present.
+    fn get_path_str<'a>(&'a self) -> Option<&'a str> {
+        self.as_ref().data.get_path_str()
+    }
+
+    /// `get_query_info` returns information about query parameters
+    fn get_query_info<'a>(&'a self) -> Option<QueryData<'a>> {
+        self.as_ref().data.get_query_info()
+    }
+}
 
 /// Url is an opque type for deserializing, and serializing Url's.
 /// Internally it uses an `Arc` to avoid additionally memory usage
@@ -55,6 +133,12 @@ impl Url {
         Ok( Url {
             data: sync::Arc::new(PrivateUrl::new(input.as_ref())?),
         }) 
+    }
+}
+impl AsRef<Url> for Url {
+    #[inline(always)]
+    fn as_ref<'a>(&'a self) -> &'a Url {
+        self
     }
 }
 impl hash::Hash for Url {
@@ -111,6 +195,7 @@ impl AsRef<str> for Url {
 impl Eq for Url { }
 unsafe impl Sync for Url { }
 unsafe impl Send for Url { }
+impl UrlOp for Url { }
 
 
 /*
