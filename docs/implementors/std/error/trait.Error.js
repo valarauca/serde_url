9eb7512be642d71542f559f296844764a2729e28
william cody laeder <codylaeder@gmail.com>
2019-11-03 13:32:48 -0800

    update docs

--- docs/implementors/std/error/trait.Error.js
+++ docs/implementors/std/error/trait.Error.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["serde"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"serde/de/value/struct.Error.html\" title=\"struct serde::de::value::Error\">Error</a>",synthetic:false,types:["serde::de::value::Error"]},];
+implementors["serde_url"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"serde_url/enum.UrlFault.html\" title=\"enum serde_url::UrlFault\">UrlFault</a>",synthetic:false,types:["serde_url::errors::UrlFault"]},];
 implementors["url"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"url/enum.ParseError.html\" title=\"enum url::ParseError\">ParseError</a>",synthetic:false,types:["url::parser::ParseError"]},];
 
             if (window.register_implementors) {
